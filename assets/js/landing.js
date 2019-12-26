5eead5c13d904d4cd5d0d3b199ef0032c6b8a475
Fernando Zhu <fernandoisadev@gmail.com>
2019-12-27 11:40:39 +1300

    Added landing page

---
+++ assets/js/landing.js
@@ -0,0 +1,6 @@
+
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+context.fillStyle = "#333333";
+context.fillRect(0, 0, canvas.clientWidth, canvas.height);
+console.log("*** CANVAS:", canvas);
