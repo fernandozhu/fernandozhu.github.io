a479bc224e9718fd22938012e10455957f9a0a54
Fernando Zhu <fernandoisadev@gmail.com>
2019-12-27 18:40:05 +1300

    Added blog page layout

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -3,4 +3,3 @@ var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 context.fillStyle = "#333333";
 context.fillRect(0, 0, canvas.clientWidth, canvas.height);
-console.log("*** CANVAS:", canvas);
